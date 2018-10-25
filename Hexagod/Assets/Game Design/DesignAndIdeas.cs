d48371343955efa4a27c720d4c35fa16ebe20d0f
Eduard Beukes <edpbeukes@gmail.com>
2018-10-25 20:35:28 +0200

    Added the game design/ideas doc to the project.

---
+++ Hexagod/Assets/Game Design/DesignAndIdeas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HexaGod
+{
+    /// <summary>
+    /// Not a real class. Just a place to store some thoughts and ideas about the project.
+    /// </summary>
+    public class DesignAndIdeas : MonoBehaviour
+    {/*
+        The game is to build tiles. And also place structures on those tiles. 
+
+            Each tile has an effect on surrounding tiles. Eg. deserts will increase the hydration of tiles surrounding it.
+
+            There has to be some compound effect with tiles stacking up. The idea is that there has to be some point of equilibrium that you have to reach between the tiles. 
+
+            Play the game by placing different shapes onto the field.
+
+
+
+            Each tile generates resources for your use. Forest generate wood for instance.
+            Mountains generate stone.
+
+            Slow paced game that expands a lot.
+
+            Craft tiles uit jou resources.
+
+            Kan tiles craft uit ander tiles.
+*/
+    }
+}
