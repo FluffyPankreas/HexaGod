3335332e69b05687f2887bf8b3bc26d11d86fcf3
Eduard Beukes <edpbeukes@gmail.com>
2018-10-25 21:03:24 +0200

    Added position functionality to the Hex class.

--- Hexagod/Assets/Scripts/HexGrids/Hex.cs
+++ Hexagod/Assets/Scripts/HexGrids/Hex.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Hexgrids
+namespace HexGrids
 {
-    public class Hex : MonoBehaviour
+    /// <summary>
+    /// This is the data structure that represents a hex. This holds information about where the hex is in the grid.
+    /// </summary>
+    public class Hex
     {
+        private Vector3Int _position = Vector3Int.zero;
+        
+        public Vector3Int Position
+        {
+            get => _position;
+            set
+            {
+                Debug.Assert(value.x + value.y + value.z == 0, "The coordinates being assigned are invalid.");
+                _position = value;
+            }
+        }
 
+        public int X 
+        {
+            get => _position.x;
+            set => Position = new Vector3Int(value, _position.y, _position.z);
+        }
+        
+        public int Y 
+        {
+            get => _position.y;
+            set => Position = new Vector3Int(_position.x, value, _position.z);
+        }
+        
+        public int Z 
+        {
+            get => _position.z;
+            set => Position = new Vector3Int(_position.x, _position.y, value);
+        }
     }
 }
