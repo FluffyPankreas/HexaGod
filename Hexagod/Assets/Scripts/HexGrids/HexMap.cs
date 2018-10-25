9e4603d0f71d7746707f5ffd25a9a4cc5b309a70
Eduard Beukes <edpbeukes@gmail.com>
2018-10-25 21:13:07 +0200

    Added the Hex Map class to the project.

---
+++ Hexagod/Assets/Scripts/HexGrids/HexMap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HexGrids
+{
+    /// <summary>
+    /// Responsible for managing the Hex data structures. Use this to translate between the hex grid positions and world positions.
+    /// </summary>
+    public class HexMap
+    {
+        private List<Hex> _grid;
+    }
+}
