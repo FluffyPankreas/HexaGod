aec112971bac24b5641db491858ae6589fa84056
Eduard Beukes <edpbeukes@gmail.com>
2018-10-27 16:17:06 +0200

    Fixed the LevelManager to be a LevelManager instead of a GameManager.

--- Hexagod/Assets/Scripts/Managers/LevelManager.cs
+++ Hexagod/Assets/Scripts/Managers/LevelManager.cs
@@ -3,26 +3,27 @@ using UnityEngine;
 
 namespace Managers
 {
+    /// <inheritdoc />
     /// <summary>
     /// Responsible for manage the state of the level. Contains the map and other level related information. 
     /// </summary>
     public class LevelManager : MonoBehaviour
     {
         
-        private static GameManager _instance;
+        private static LevelManager _instance;
         /// <summary>
         /// The static reference to the Game Manager. 
         /// </summary>
-        public static GameManager Instance
+        public static LevelManager Instance
         {
             get
             {
                 if (_instance == null)
                 {
                     GameObject newManager = new GameObject();
-                    newManager.AddComponent<GameManager>();
+                    newManager.AddComponent<LevelManager>();
 
-                    _instance = newManager.GetComponent<GameManager>();
+                    _instance = newManager.GetComponent<LevelManager>();
                 }
 
                 return _instance;
@@ -31,9 +32,15 @@ namespace Managers
 
         private HexMap _levelMap;
 
-        public HexMap LevelMap
+        public HexMap LevelMap => _levelMap;
+
+        /// <summary>
+        /// Adds the tile to the map.
+        /// </summary>
+        /// <param name="tileToAdd">The tile that will be added to the map.</param>
+        public void AddTileToMap(Hex tileToAdd)
         {
-            get => _levelMap;
+            //TODO: Implement functionality for this.
         }
     }
 }
