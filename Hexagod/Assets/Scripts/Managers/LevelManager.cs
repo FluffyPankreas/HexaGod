0e31f3e9542334f44d100fb487d8855602e7fc63
Eduard Beukes <edpbeukes@gmail.com>
2018-10-27 16:02:00 +0200

    Added the LevelManager component to the project.

---
+++ Hexagod/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using HexGrids;
+using UnityEngine;
+
+namespace Managers
+{
+    /// <summary>
+    /// Responsible for manage the state of the level. Contains the map and other level related information. 
+    /// </summary>
+    public class LevelManager : MonoBehaviour
+    {
+        
+        private static GameManager _instance;
+        /// <summary>
+        /// The static reference to the Game Manager. 
+        /// </summary>
+        public static GameManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    GameObject newManager = new GameObject();
+                    newManager.AddComponent<GameManager>();
+
+                    _instance = newManager.GetComponent<GameManager>();
+                }
+
+                return _instance;
+            }
+        }
+
+        private HexMap _levelMap;
+
+        public HexMap LevelMap
+        {
+            get => _levelMap;
+        }
+    }
+}
