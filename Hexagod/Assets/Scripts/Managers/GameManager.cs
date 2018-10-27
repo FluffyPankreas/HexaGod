73f99c36c84d3ac0a2537c9939d084ac0142e502
Eduard Beukes <edpbeukes@gmail.com>
2018-10-27 14:46:50 +0200

    Added the GameManager component to the project.

---
+++ Hexagod/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Managers
+{
+    /// <summary>
+    /// This component is responsible for managing the game state. This includes keeping track of what scene the player is in.
+    /// </summary>
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager _instance;
+        public static GameManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    GameObject newManager = new GameObject();
+                    newManager.AddComponent<GameManager>();
+
+                    _instance = newManager.GetComponent<GameManager>();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
